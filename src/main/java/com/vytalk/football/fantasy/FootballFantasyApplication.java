8501299dbe0be47e6610de69bde0a97132ac3a6d
Vytautas Alkimavicius <vytautas.alkimavicius@metasite.net>
2020-03-11 15:28:54 +0200

    Initial commit

---
+++ src/main/java/com/vytalk/football/fantasy/FootballFantasyApplication.java
@@ -0,0 +1,13 @@
+package com.vytalk.football.fantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFantasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballFantasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vytalk/football/fantasy/FootballFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vytalk.football.fantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballFantasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
